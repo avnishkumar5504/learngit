76966182283a94635667ecee143186ac4048cef5
avnishkumar5504 <avnishkurmi2005@gmail.com>
2026-05-01 14:17:01 +0530

    add diff file

---
+++ diff.java
@@ -0,0 +1,8 @@
+public class diff {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int diff=a-b;
+        System.out.println("The difference of a and b is: " + diff);
+    }
+}
