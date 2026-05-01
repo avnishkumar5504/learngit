cb8cd0b71d11991b47c50f8d7eb3f3ff6dee70ac
avnishkumar5504 <avnishkurmi2005@gmail.com>
2026-05-01 15:13:34 +0530

    adding somthing

--- diff.java
+++ diff.java
@@ -4,5 +4,6 @@ public class diff {
         int b=20;
         int diff=a-b;
         System.out.println("The difference of a and b is: " + diff);
+        System.out.println("I want to check somthing this is working or not");
     }
 }
