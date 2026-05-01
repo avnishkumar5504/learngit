b470457ae453b043d37d2e6c60c6a57509f0e57b
avnishkumar5504 <avnishkurmi2005@gmail.com>
2026-05-01 14:41:05 +0530

    add multiply files

---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class multiply {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int product=a*b;
+        System.out.println("The product of a and b is: " + product);
+    }
+}
